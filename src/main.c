713430a9361894036cad162798cf4566e1cd32bd
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-16 22:33:23 -0700

    horizontal ball done

--- src/main.c
+++ src/main.c
@@ -2,11 +2,18 @@
 // TODO: delta time
 
 int gravity = 1;
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 1000;
 
 typedef struct Velocity {
     int x;
     int y;
 } Velocity;
+
+typedef struct Accel {
+    int x;
+    int y;
+} Accel;
 
 typedef struct Circle {
     int x;
@@ -14,19 +21,28 @@ typedef struct Circle {
     float r;
     Velocity vel;
     Color col;
+    Accel accl;
 } Circle;
 
 void update_shapes(Circle* c) {
     c->x += c->vel.x;
     c->y += c->vel.y;
-    if(c->y >= 800) {
+    if(c->y >= WINDOW_HEIGHT) {
         c->vel.y = -10; // bounce factor
-        gravity = -1;
+        c->accl.y = -1;
     } else if(c->y <= 0) {
         c->vel.y = 10; // bounce factor
-        gravity = 1;
+        c->accl.y = 1;
     }
-    c->vel.y += gravity;
+    if(c->x >= WINDOW_WIDTH) {
+        c->vel.x = -10; // bounce factor
+        c->accl.x = -1;
+    } else if(c->x <= 0) {
+        c->vel.x = 10; // bounce factor
+        c->accl.x = 1;
+    }
+    c->vel.x += c->accl.x;
+    c->vel.y += c->accl.y;
 }
 
 int main(void) {
@@ -37,14 +53,28 @@ int main(void) {
     c.r = 50.0f;
     c.vel.x = 0;
     c.vel.y = 0;
+    c.accl.x = 0;
+    c.accl.y = 1;
     c.col = BLACK;
 
-    InitWindow(800, 800, "Shaking Game");
+    Circle c2;
+    c2.x = 0;
+    c2.y = WINDOW_HEIGHT/2;
+    c2.r = 50.0f;
+    c2.vel.x = 0;
+    c2.vel.y = 0;
+    c2.accl.x = 1;
+    c2.accl.y = 0;
+    c2.col = BLUE;
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Shaking Game");
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         update_shapes(&c);
         DrawCircle(c.x, c.y, c.r, c.col);
+        update_shapes(&c2);
+        DrawCircle(c2.x, c2.y, c2.r, c2.col);
         EndDrawing();
     }
     CloseWindow();
