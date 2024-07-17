4c9a97a994689f0c904e01e715f1cc32b5bb67c7
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-16 21:50:28 -0700

    removed build dir

--- build/CMakeFiles/Shaking_Game.dir/src/main.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Shaking_Game.dir/src/main.c.o: \
-  /Users/meruzhansargsyan/Desktop/temp/Coding/Projects/Shaking_Game/src/main.c \
-  /Users/meruzhansargsyan/Desktop/temp/Coding/Projects/Shaking_Game/build/_deps/raylib-src/src/raylib.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/15.0.0/include/stdarg.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/15.0.0/include/stdbool.h
