44c848f6d4bb04225ec27506294ca9062bfad6a6
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-16 18:43:37 -0700

    circle drawn

--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
 #include <raylib.h>
 
+typedef struct Circle {
+    int x;
+    int y;
+    float r;
+    Color col;
+} Circle;
+
 int main(void) {
     SetTargetFPS(60);
+    Circle c;
+    c.x = 250;
+    c.y = 250;
+    c.r = 50.0f;
+    c.col = BLACK;
 
-    InitWindow(500, 500, "Shaking Game");
+    InitWindow(800, 800, "Shaking Game");
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawCircle(c.x, c.y, c.r, c.col);
         EndDrawing();
     }
     CloseWindow();
