2455baf6c6aac5a89478a2eea499a5740101f54b
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-20 15:36:16 -0700

    delta time & collision added

--- src/main.c
+++ src/main.c
@@ -14,9 +14,9 @@ typedef struct Circle {
     Color col;
 } Circle;
 
-void update_shapes(Circle* c) {
-    c->pos.x += c->vel.x;
-    c->pos.y += c->vel.y;
+void update_shapes(Circle* c, float deltaTime) {
+    c->pos.x += c->vel.x * deltaTime;
+    c->pos.y += c->vel.y * deltaTime;
     if(c->pos.y >= WINDOW_HEIGHT) {
         c->vel.y = -10; // bounce factor
         c->accl.y = -1;
@@ -31,41 +31,39 @@ void update_shapes(Circle* c) {
         c->vel.x = 10; // bounce factor
         c->accl.x = 1;
     }
-    c->vel.x += c->accl.x;
-    c->vel.y += c->accl.y;
+    c->vel.x += c->accl.x * deltaTime;
+    c->vel.y += c->accl.y * deltaTime;
+}
+
+Circle get_circle(Vector2 pos, float rad, Vector2 vel, Vector2 acc, Color col) {
+    Circle c = {pos, vel, acc, rad, col};
+    return c;
 }
 
 int main(void) {
     SetTargetFPS(60);
-    Circle c;
-    c.pos.x = 250;
-    c.pos.y = 250;
-    c.r = 50.0f;
-    c.vel.x = 0;
-    c.vel.y = 0;
-    c.accl.x = 0;
-    c.accl.y = 1;
-    c.col = BLACK;
+    Vector2 vel = {0, 20};
+    Vector2 pos = {250, 250};
+    Vector2 acc = {0, 1};
+    float r = 60.0f;
+    Circle c = get_circle(pos, r, vel, acc, BLACK);
 
-    Circle c2;
-    c2.pos.x = 0;
-    c2.pos.y = WINDOW_HEIGHT/2;
-    c2.r = 50.0f;
-    c2.vel.x = 0;
-    c2.vel.y = 0;
-    c2.accl.x = 1;
-    c2.accl.y = 0;
-    c2.col = BLUE;
+    Vector2 vel2 = {20, 0};
+    Vector2 pos2 = {0, WINDOW_HEIGHT/2};
+    Vector2 acc2 = {0, 0};
+    float r2 = 50.0f;
+    Circle c2 = get_circle(pos2, r2, vel2, acc2, BLUE);
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Shaking Game");
     while(!WindowShouldClose()) {
+        float deltaTime = GetFrameTime();
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        update_shapes(&c);
-        update_shapes(&c2);
         if(CheckCollisionCircles(c.pos, c.r, c2.pos, c2.r)) {
-            printf("touching");
-        }
+            c.col = RED;
+        } else {c.col = BLACK;}
+        ClearBackground(RAYWHITE);
+        update_shapes(&c, deltaTime);
+        update_shapes(&c2, deltaTime);
         DrawCircle(c.pos.x, c.pos.y, c.r, c.col);
         DrawCircle(c2.pos.x, c2.pos.y, c2.r, c2.col);
         EndDrawing();
