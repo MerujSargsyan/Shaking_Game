fd34b7a57c422028332dbc4468d4e55305c5fcc3
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-21 09:37:20 -0700

    saving this version

---
+++ build/_deps/raylib-build/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/meruzhansargsyan/Desktop/temp/Coding/Projects/Shaking_Game/build/_deps/raylib-src/src
+# Build directory: /Users/meruzhansargsyan/Desktop/temp/Coding/Projects/Shaking_Game/build/_deps/raylib-build/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
-#include <raylib.h>
 #include <stdio.h>
+#include <raylib.h>
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 1000;
-
-int frame_since_phys = 0;
-int next_phys_check = 25;
+const float PHYSICS_TIME_STEP = 1.0f / 60.0f; // 60 checks per second
 
 typedef struct Circle {
     int mass;
@@ -16,17 +14,10 @@ typedef struct Circle {
     Color col;
 } Circle;
 
-bool time_to_check() {
-    if(frame_since_phys > next_phys_check) {
-        frame_since_phys = 0;
-        return true;
-    }
-    return false;
-}
-
 void update_shapes(Circle* c, float deltaTime) {
     c->pos.x += c->vel.x * deltaTime;
     c->pos.y += c->vel.y * deltaTime;
+    
     if(c->pos.y >= WINDOW_HEIGHT || c->pos.y <= 0) {
         c->vel.y = -c->vel.y;
     }
@@ -45,39 +36,42 @@ Circle get_circle(int mass, Vector2 pos, float rad, Vector2 vel, Vector2 acc, Co
 }
 
 void simulate_physics(Circle* c1, Circle* c2) {
-    if(c1->mass > c2->mass) {
-        c1->vel.x -= c2->vel.x/c2->mass;        
-        c1->vel.y -= c2->vel.y/c2->mass;        
-        c2->vel.x *= -1;
-        c2->vel.y *= -1;
-    }
+    Vector2 diff = Vector2Subtract(c1->vel, c2->vel);
+    printf("x: %f, y: %f\n", diff.x, diff.y);
 }
 
 int main(void) {
     SetTargetFPS(60);
-    Vector2 vel = {0, 80};
+    Vector2 vel = {0, 100};
     Vector2 pos = {250, 250};
-    Vector2 acc = {0, 10};
+    Vector2 acc = {0, 0};
     Circle c = get_circle(50, pos, 60.0f, vel, acc, BLACK);
 
-    Vector2 vel2 = {70, 0};
+    Vector2 vel2 = {100, 0};
     Vector2 pos2 = {0, WINDOW_HEIGHT/2};
-    Vector2 acc2 = {10, 0};
+    Vector2 acc2 = {0, 0};
     Circle c2 = get_circle(30, pos2, 50.0f, vel2, acc2, BLUE);
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Shaking Game");
+
+    float accumulator = 0.0f;
     while(!WindowShouldClose()) {
         float deltaTime = GetFrameTime();
-        frame_since_phys++;
+        accumulator += deltaTime;
+
+        while(accumulator >= PHYSICS_TIME_STEP) {
+            if(CheckCollisionCircles(c.pos, c.r, c2.pos, c2.r)) {
+                simulate_physics(&c, &c2);
+            }
+
+            update_shapes(&c, deltaTime);
+            update_shapes(&c2, deltaTime);
+
+            accumulator -= PHYSICS_TIME_STEP;
+        }
+        
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
-        if(time_to_check() && CheckCollisionCircles(c.pos, c.r, c2.pos, c2.r)) {
-            simulate_physics(&c, &c2);
-        }
-
-        update_shapes(&c, deltaTime);
-        update_shapes(&c2, deltaTime);
         DrawCircle(c.pos.x, c.pos.y, c.r, c.col);
         DrawCircle(c2.pos.x, c2.pos.y, c2.r, c2.col);
 
