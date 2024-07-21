dec7c1154e5449cfa53c5a963d293758213d472b
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-21 09:38:37 -0700

    keep making this mistake

--- build/CMakeFiles/physics.dir/src/physics.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/physics.dir/src/physics.c.o: \
-  /Users/meruzhansargsyan/Desktop/temp/Coding/Projects/Shaking_Game/src/physics.c \
-  /Users/meruzhansargsyan/Desktop/temp/Coding/Projects/Shaking_Game/include/physics.h \
-  /Users/meruzhansargsyan/Desktop/temp/Coding/Projects/Shaking_Game/build/_deps/raylib-src/src/raylib.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/15.0.0/include/stdarg.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/15.0.0/include/stdbool.h
