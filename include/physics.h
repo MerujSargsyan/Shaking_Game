a9ef32243f53a905e3d648de922807ec6757efd7
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-21 21:17:21 -0700

    refactor and vector logic good

--- include/physics.h
+++ include/physics.h
@@ -5,7 +5,17 @@
 #include <stdio.h>
 #include <raymath.h>
 
-Vector2 vec_add(Vector2 v1, Vector2 v2);
+typedef struct Circle {
+    int mass;
+    Vector2 pos;
+    Vector2 vel;
+    Vector2 accl;
+    float rad;
+    Color col;
+} Circle;
+
+Circle get_circle(int mass, Vector2 pos, Vector2 vel, Vector2 acc, float rad, Color col);
+void simulate_physics(Circle* c1, Circle* c2);
 void print_vec(Vector2 v);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,14 +5,7 @@ const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 1000;
 const float PHYSICS_TIME_STEP = 1.0f / 60.0f; // 60 checks per second
 
-typedef struct Circle {
-    int mass;
-    Vector2 pos;
-    Vector2 vel;
-    Vector2 accl;
-    float r;
-    Color col;
-} Circle;
+
 
 void update_shapes(Circle* c, float deltaTime) {
     c->pos.x += c->vel.x * deltaTime;
@@ -30,27 +23,19 @@ void update_shapes(Circle* c, float deltaTime) {
     c->vel.y += c->accl.y * deltaTime;
 }
 
-Circle get_circle(int mass, Vector2 pos, float rad, Vector2 vel, Vector2 acc, Color col) {
-    Circle c = {mass, pos, vel, acc, rad, col};
-    return c;
-}
 
-void simulate_physics(Circle* c1, Circle* c2) {
-    Vector2 diff = Vector2Subtract(c1->vel, c2->vel);
-    print_vec(diff);
-}
 
 int main(void) {
     SetTargetFPS(60);
     Vector2 vel = {0, 100};
     Vector2 pos = {250, 250};
     Vector2 acc = {0, 0};
-    Circle c = get_circle(50, pos, 60.0f, vel, acc, BLACK);
+    Circle c = get_circle(50, pos, vel, acc, 60.0f, BLACK);
 
     Vector2 vel2 = {100, 0};
     Vector2 pos2 = {0, WINDOW_HEIGHT/2};
     Vector2 acc2 = {0, 0};
-    Circle c2 = get_circle(30, pos2, 50.0f, vel2, acc2, BLUE);
+    Circle c2 = get_circle(30, pos2, vel2, acc2, 50.0f, BLUE);
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Shaking Game");
 
@@ -60,7 +45,7 @@ int main(void) {
         accumulator += deltaTime;
 
         while(accumulator >= PHYSICS_TIME_STEP) {
-            if(CheckCollisionCircles(c.pos, c.r, c2.pos, c2.r)) {
+            if(CheckCollisionCircles(c.pos, c.rad, c2.pos, c2.rad)) {
                 simulate_physics(&c, &c2);
             }
 
@@ -72,8 +57,8 @@ int main(void) {
         
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircle(c.pos.x, c.pos.y, c.r, c.col);
-        DrawCircle(c2.pos.x, c2.pos.y, c2.r, c2.col);
+        DrawCircle(c.pos.x, c.pos.y, c.rad, c.col);
+        DrawCircle(c2.pos.x, c2.pos.y, c2.rad, c2.col);
 
         EndDrawing();
     }
